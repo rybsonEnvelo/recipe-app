fef430a38fcf9ddcd4c80d508dae64b4c793878b
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-03-29 11:27:59 +0200

    add: all components
    style: imported styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+import { RecipeTileComponent } from './recipe-tile/recipe-tile.component';
+import { RecipeFormComponent } from './recipe-form/recipe-form.component';
+import { RecipeDescriptionComponent } from './recipe-description/recipe-description.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    RecipeListComponent,
+    RecipeTileComponent,
+    RecipeFormComponent,
+    RecipeDescriptionComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
