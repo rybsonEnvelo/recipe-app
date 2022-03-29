22e86cf977ebd357b0db1be0ce088e89bd0fef68
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-03-29 12:27:16 +0200

    add: SortOption interface, SortService and injected to component

--- src/app/recipe-list/recipe-list.component.ts
+++ src/app/recipe-list/recipe-list.component.ts
@@ -1,15 +1,16 @@
 import { Component, OnInit } from '@angular/core';
+import { SortOption } from '../interfaces/SortOption';
+import { SortService } from './sort.service';
 
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
-  styleUrls: ['./recipe-list.component.scss']
+  styleUrls: ['./recipe-list.component.scss'],
 })
 export class RecipeListComponent implements OnInit {
+  sortOptions: SortOption[] = this.sortService.getSortOptions();
 
-  constructor() { }
+  constructor(private sortService: SortService) {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
