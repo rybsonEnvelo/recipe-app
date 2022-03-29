c63336fc62e0839059d8690ca3248d603204becd
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-03-29 15:14:53 +0200

    test: added test to pipes

--- src/app/shared/first-letter-upper-case.pipe.spec.ts
+++ src/app/shared/first-letter-upper-case.pipe.spec.ts
@@ -1,8 +1,13 @@
 import { FirstLetterUpperCasePipe } from './first-letter-upper-case.pipe';
 
 describe('FirstLetterUpperCasePipe', () => {
-  it('create an instance', () => {
+  it('shouldBe Jajecznica', () => {
     const pipe = new FirstLetterUpperCasePipe();
-    expect(pipe).toBeTruthy();
+    expect(pipe.transform('jajecznica')).toBe('Jajecznica');
+  });
+
+  it('shouldBe Jajecznica z pomidorami', () => {
+    const pipe = new FirstLetterUpperCasePipe();
+    expect(pipe.transform('jajecznica z pomidorami')).toBe('Jajecznica z pomidorami');
   });
 });
