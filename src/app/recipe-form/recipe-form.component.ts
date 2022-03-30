89173713c43f6d6c3818e48cb38ea0d6e1b3b99c
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-03-30 16:26:10 +0200

    add: form elements, style: form

--- src/app/recipe-form/recipe-form.component.ts
+++ src/app/recipe-form/recipe-form.component.ts
@@ -1,15 +1,44 @@
 import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Ingredient } from '../interfaces/Ingredient';
 
 @Component({
   selector: 'app-recipe-form',
   templateUrl: './recipe-form.component.html',
-  styleUrls: ['./recipe-form.component.scss']
+  styleUrls: ['./recipe-form.component.scss'],
 })
 export class RecipeFormComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  form!: FormGroup;
+  ingredientFormGroup(): FormGroup {
+    return this.formBuilder.group({
+      ingredientName: ['', Validators.required, Validators.minLength(3)],
+      ingredientValue: ['', Validators.required],
+    });
   }
 
+  get ingredients() {
+    return this.form.get('ingredients') as FormArray;
+  }
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.form = this.formBuilder.group({
+      name: ['', Validators.required, Validators.minLength(3)],
+      description: ['', Validators.required, Validators.minLength(16), , Validators.max(1500)],
+      ingredients: this.formBuilder.array([this.ingredientFormGroup()]),
+    });
+  }
+
+  addIngredient() {
+    this.ingredients.push(this.ingredientFormGroup());
+  }
+
+  removeIngredient(index: number) {
+    this.ingredients.removeAt(index);
+  }
+
+  onSubmit() {
+    console.log(this.form.value);
+  }
 }
