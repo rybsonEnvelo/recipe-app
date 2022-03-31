1206e20c647db0ca4725db179622674c0c730c6a
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-03-31 18:58:45 +0200

    add: modal elements, style: modal

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterContentInit, AfterViewInit, Component, ElementRef, OnInit, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
-  styleUrls: ['./modal.component.scss']
+  styleUrls: ['./modal.component.scss'],
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent {
+  @ViewChildren('star') stars = new QueryList<ElementRef>();
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  setRating(currentStar: number) {
+    this.stars.forEach((star, index) => {
+      if (index <= currentStar) star.nativeElement.firstChild.classList.remove('inactive');
+      else star.nativeElement.firstChild.classList.add('inactive');
+    });
   }
-
 }
