7775a336ca14f4db9370ac121d4b392524328aec
Mariusz Rybski <mariusz.rybski@ppuc.pl>
2022-04-02 14:57:23 +0200

    add: loginService, add: login validation

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../shared/auth.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  loginForm!: FormGroup;
+  isLoginError: boolean = false;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder, private authService: AuthService) {}
 
   ngOnInit(): void {
+    this.createForm();
   }
 
+  createForm() {
+    this.loginForm = this.formBuilder.group({
+      email: ['', [Validators.required, Validators.pattern(this.authService.regex)]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    console.log(this.loginForm.value);
+  }
 }
---
+++ src/app/shared/auth.service.ts
@@ -0,0 +1,11 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  public regex =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  constructor() {}
+}
